8b9dbcda77229bd37e021ab010bef47ff3b8a906
Tejvir Mann <tejvirmann@Tejvirs-MBP.lan>
2024-08-18 18:12:15 -0500

    fixes

--- src/components/Homepage/Ens.tsx
+++ src/components/Homepage/Ens.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import styles from './ens.module.scss'
 import Link from '@docusaurus/Link'
 
-export default function Ens() {
+// Define the interface for the component's props
+interface EnsProps {
+  title: string
+  subtitle: string
+  description: string
+  imageSrc: string
+  furtherDesc: string
+  button: string
+}
+
+export default function Ens({ title, subtitle, description, imageSrc, furtherDesc, button }: EnsProps) {
   return (
     <section className={'relative my-8 md:my-12 group ' + styles.sectionBg}>
       <div className="absolute inset-0 w-full h-full">
-        <img className={'w-full h-full object-cover ' + styles.coverImg} src="/img/home/dishwasher.png" alt="" />
+        <img className={'w-full h-full object-cover ' + styles.coverImg} src={imageSrc} alt="" />
       </div>
 
       <div className={'absolute w-full top-0 h-4 p-3 transition-all duration-150 bg-gradient-to-b ' + styles.overlay} />
@@ -28,18 +38,16 @@ export default function Ens() {
 
       <div className="relative max-w-4xl mx-auto text-center py-16 lg:py-24 px-8 sm:px-12 lg:px-3">
         <h3 className="text-3xl md:text-5xl uppercase pb-4 tracking-widest">
-          Dishwasher <span className="block pt-1 text-xl md:text-3xl tracking-normal font-light">THE ANIME</span>
+          {title} <span className="block pt-1 text-xl md:text-3xl tracking-normal font-light">{subtitle}</span>
         </h3>
         <div className="text-base sm:text-xl lg:text-lg xl:text-xl">
-          <p>Dishwasher is an anime that I am creating.</p>
-          <p>
-            I am currently working on assembling a production team and learning all I can about animation and film. If you are interested please reach out!
-          </p>
+          <p>{description}</p>
+          <p>{furtherDesc}</p>
         </div>
         <div className="pt-8">
           <Link className="" to="/logs/projects/dishwasher">
-            <span className="inline-block px-6 py-3 rounded-full bg-pink-500 hover:bg-white hover:shadow-lg hover:shadow-pink-500 text-base font-medium text-white hover:text-pink-500 transition-all duration-300 hover:-translate-y-1">
-              Learn More
+            <span className="inline-block px-6 py-3 rounded-full bg-green-500 hover:bg-white hover:shadow-lg hover:shadow-green-500 text-base font-medium text-white hover:text-green-500 transition-all duration-300 hover:-translate-y-1">
+              {button}
             </span>
           </Link>
         </div>
